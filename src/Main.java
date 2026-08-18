f66711da20b74fe42da2ef6cdca58b6dad94ca93
RoderikCL <erikcl@hotmail.com>
2026-08-18 17:06:34 -0600

    Primer programa de Tiendita JAva

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+package src;
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("================================");
+        System.out.println("        TIENDITA JAVA");
+        System.out.println("================================");
+
+        String nombreProducto = "Coca 600";
+            int cantidad = 12;
+            double precioVenta = 27.00;
+            double precioCompra = 17.08;
+        
+
+        System.out.println("Producto: " + nombreProducto);
+            System.out.println("Stock: " + cantidad);
+            System.out.println("Precio de venta: $" + precioVenta);
+            System.out.println("Precio de compra: $" + precioCompra);
+    }
+}
---
+++ src/Producto.java
@@ -0,0 +1,10 @@
+package src;
+
+public class Producto {
+
+    String nombreProducto = "Coca 355";
+            int cantidad = 19;
+            double precioVenta = 17.00;
+            double precioCompra = 12.17;
+    
+}
